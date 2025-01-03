20c7abdc650637b3a86742b5636e85ba3b4c14eb
Lht97 <lhtian97@gmail.com>
2025-01-03 23:42:43 +0800

    Change the profile_optiprofiler_script.m

--- tests/profile_optiprofiler_script.m
+++ tests/profile_optiprofiler_script.m
@@ -1,16 +1,14 @@
 clear all
-% options.dim = 'big';
-options.mindim = 1;
-options.maxdim = 1;
-% options.n_jobs = 1;
-options.feature_name = 'noisy_1e-3';
-options.solver_names = {'pbds', 'cbds-orig'};
-profile_optiprofiler(options);
+options.dim = 'big';
+options.solver_names = {'cbds', 'nomad'};
+feature = {'perturbed_x0_0.001', 'perturbed_x0_1', 'perturbed_x0_10', 'perturbed_x0_100', 'truncated_1', 'truncated_2', 'truncated_3', 'truncated_4', 'permuted', 'random_nan_5', 'random_nan_10', 'random_nan_20'};
+for i = 1:length(feature)
+    options.feature_name = feature{i};
+    profile_optiprofiler(options);
+end
 
-% options.solver_names = {'cbds', 'nomad'};
-% profile_optiprofiler(options);
 
-% options.feature_name = 'linearly_transformed';
+% options.feature_name = 'perturbed_x0_1';
 % profile_optiprofiler(options);
 
 % options.feature_name = 'noisy_1e-1';
